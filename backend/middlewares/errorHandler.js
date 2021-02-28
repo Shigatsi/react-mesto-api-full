0cd5d42f72a130845675f19de50f86d631fe6ce9
Shigatsi <shigatsii@gmail.com>
2021-02-28 22:47:30 +0300

    fix: part of error fixed

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -6,8 +6,8 @@ const errorHandler = (err, req, res, next) => {
   if (err instanceof CelebrateError) {
     res.status(400).send(err.details.get('body'));
   }
-  if (err.status) {
-    res.status(err.status).send({ message: err.message });
+  if (err.statusCode) {
+    res.status(err.statusCode).send({ message: err.message });
   }
 
   res.status(500).send({ message: err.message });
